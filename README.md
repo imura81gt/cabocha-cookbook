633836a1310d61e585f63bd5167fdde39f894af7
imura <imura@realworld.jp>
2015-04-22 20:26:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cabocha
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cabocha'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures cabocha'
+long_description 'Installs/Configures cabocha'
+version          '0.1.0'
+
